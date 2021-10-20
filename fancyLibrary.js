9c1de81920ed3959b3f30f0c00507f6a2470135d
Steven Moyano <stevenmoyano1@gmail.com>
2021-10-20 18:01:36 -0500

    completed required tasks

--- fancyLibrary.js
+++ fancyLibrary.js
@@ -1,23 +1,22 @@
-function dataProvider(selectorFuction){
-  const newData = { name: 'person' }
+function dataProvider(selectorFunction) {
+  const newData = { name: "person" };
 
   setTimeout(() => {
-    selectorFuction(newData);
+    selectorFunction(newData);
   }, 3000);
 }
 
+var fetchData = () =>
+  new Promise((resolve, reject) => {
+    console.log("fetchingData from imaginary database");
+    setTimeout(() => {
+      try {
+        // fetchingData from imaginary database
+        resolve({ name: "john", age: 42 });
+      } catch (error) {
+        reject(error);
+      }
+    }, 2000);
+  });
 
-var fetchData = () => new Promise((resolve, reject) => {
-  console.log('fetchingData from imaginary database')
-  setTimeout(() => {
-       try {
-         // fetchingData from imaginary database
-         resolve({name: 'john', age:42})
-        } catch(error) {
-          reject(error);
-        }
-  }, 5000);
-});
-
-
-module.exports =  { dataProvider, fetchData };
+module.exports = { dataProvider, fetchData };
